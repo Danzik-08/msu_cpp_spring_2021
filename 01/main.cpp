7edb0bec7af34439c4c1f2a5ef98c42d256f523c
Danzan Dedinov <dedinov@list.ru>
2021-05-11 00:09:22 +0300

    fix Makefile and add unit-test in main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,68 +1,54 @@
 #include "Allocator.hpp"
 #include <iostream>
+#include <cassert>
 
-void	checkMakeAllocator(size_t test1)
+void	testMakeAllocator(size_t test1)
 {
 	Allocator	all;
 	char		*res;
 
-	std::cout << "----------CHECK_MAKE_ALLOCATOR---------\n";
 	all.makeAllocator(test1);
 	res = all.alloc(test1);
 	for (size_t i = 0; i < test1 - 1; ++i)
 		res[i] = 'a' + i % 26;
 	res[test1 - 1] = '\0';
-	std::cout << res << '\n';
 	res = all.alloc(1);
-	if (res == nullptr)
-		std::cout << "makeAllocator alloced " << test1 << " bytes - GOOD\n";
-	else
-		std::cout << ":(\n";
+	assert(res == nullptr);
 	all.makeAllocator(1);
-	if (all.alloc(2) == nullptr)
-		std::cout << "makeAllocator realloced memory - GOOD\n";
+	assert(all.alloc(2) == nullptr);
 }
 
-void	checkAlloc()
+void	testAlloc()
 {
 	size_t		size = 100;
 	Allocator	all;
 	char		*res;
 
-	std::cout << "----------CHECK_ALLOC---------\n";
 	all.makeAllocator(size);
 	for (int i = 0; i < 10; ++i)
-		if (all.alloc(10) == nullptr)
-		{
-			std::cout << "BAD\n";
-			return ;
-		}
-	if (all.alloc(1) == nullptr)
-		std::cout << "alloc returned nullptr and every time worked correctly - GOOD\n";
+		assert(all.alloc(10) != nullptr);
+	assert(all.alloc(1) == nullptr);
 }
 
-void	checkReset()
+void	testReset()
 {
 	Allocator	all;
 
-	std::cout << "----------CHECK_RESET---------\n";
 	all.makeAllocator(1);
 	for (int i = 0; i < 10; ++i)
 	{
 		all.alloc(1);
 		all.reset();
-		if (all.alloc(1) == nullptr)
-		{
-			std::cout << "Reset doesn't work correctly\n";
-			return ;
-		}
+		assert(all.alloc(1) != nullptr);
 	}
-	std::cout <<"Reset work correctly\n";
 }
 
 int main()
 {
-	checkMakeAllocator(100);
-	checkAlloc();
-	checkReset();
+	testMakeAllocator(100);
+	std::cout << "MakeAllocator() - success\n";
+	testAlloc();
+	std::cout << "Alloc() - success\n";
+	testReset();
+	std::cout << "Reset() - success\n";
 }
